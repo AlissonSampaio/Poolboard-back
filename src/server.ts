fdd300ed3a639810e68fbada6499a8e2d5d8f338
Alisson Alves <alisson2012alves@gmail.com>
2022-08-11 16:32:46 -0300

    Configurando porta do heroku

--- src/server.ts
+++ src/server.ts
@@ -5,39 +5,41 @@ import { routes } from "./routes";
 import cors from "cors";
 
 AppDataSource.initialize()
-    .then(() => {
-        console.log("Data Source has been initialized!")
-    })
-    .catch((err) => {
-        console.error("Error during Data Source initialization", err)
-    })
- 
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization", err);
+  });
+
 const app = express();
 
 const corsOptions = () => {
-    const allowList = String("http://192.168.100.117:3000,http://localhost:3000,http://http://172.16.3.50:3000/")
-        .split(',')
-        .map(domain => domain.trim());
+  const allowList = String(
+    "http://192.168.100.117:3000,http://localhost:3000,http://http://172.16.3.50:3000/"
+  )
+    .split(",")
+    .map((domain) => domain.trim());
 
-    return  {
-        origin: function (origin: any, callback: Function) {
-            if (allowList.indexOf(origin) !== -1 || !origin) {
-                return callback(null, true)
-            }
-            return callback(new Error('Not allowed by CORS'));
-        },
-        exposedHeaders: 'Date',
+  return {
+    origin: function (origin: any, callback: Function) {
+      if (allowList.indexOf(origin) !== -1 || !origin) {
+        return callback(null, true);
       }
-}
+      return callback(new Error("Not allowed by CORS"));
+    },
+    exposedHeaders: "Date",
+  };
+};
 
 app.use(express.json());
 
 app.use(cors());
 
-app.options('*', cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(routes);
 
-app.listen(666, ()=> {
-    console.log("Server started");
+app.listen(process.env.PORT || 666, () => {
+  console.log("Server started");
 });
