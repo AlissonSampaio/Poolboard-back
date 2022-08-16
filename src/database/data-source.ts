c1b50188d7577f80d3bdda8355892556f6e180ea
Alisson Alves <alisson2012alves@gmail.com>
2022-08-16 11:16:01 -0300

    Testes de deploy no heroku

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -2,7 +2,7 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
+  host: "192.168.100.117",
   port: 5432,
   username: "postgres",
   password: "root",
