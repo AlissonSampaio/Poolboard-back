9f784b6e6ec5a9ca5293c625a424a72444870838
Alisson Alves <alisson2012alves@gmail.com>
2022-08-16 10:06:41 -0300

    Atualizando porta back-end

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,14 +1,14 @@
-import { DataSource } from "typeorm"
+import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "root",
-    database: "poolboard",
-    entities: ["src/entities/*.{js,ts}"],
-    migrations:["src/database/migrations/*.ts"],
-    logging: false,
-    synchronize: true
-})
+  type: "postgres",
+  host: "https://poolboard-hub-backend.herokuapp.com/",
+  port: 5432,
+  username: "postgres",
+  password: "root",
+  database: "poolboard",
+  entities: ["src/entities/*.{js,ts}"],
+  migrations: ["src/database/migrations/*.ts"],
+  logging: false,
+  synchronize: true,
+});
