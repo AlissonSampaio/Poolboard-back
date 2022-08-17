a33b92fd6151d848851b3c08896c12ac02962143
Alisson Alves <alisson2012alves@gmail.com>
2022-08-17 15:59:59 -0300

    Testes no heroku

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,14 +1,33 @@
 import { DataSource } from "typeorm";
 
+// export const AppDataSource = new DataSource({
+//   type: "postgres",
+//   host: "localhost",
+//   port: 5432,
+//   username: "postgres",
+//   password: "root",
+//   database: "poolboard",
+//   entities: ["src/entities/*.{js,ts}"],
+//   migrations: ["src/database/migrations/*.ts"],
+//   logging: false,
+//   synchronize: true,
+// });
+
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "192.168.100.117",
-  port: 5432,
-  username: "postgres",
-  password: "root",
-  database: "poolboard",
+  // host: "ec2-34-231-63-30.compute-1.amazonaws.com",
+  // port: 5432,
+  // username: "ueyafgxlxzldeh",
+  // password: "76ae13ca1840914ab869e18438fd0280750cd6566cc0e8f73dc588f84e016626",
+  // database: "ddfqh165llskfc",
+  url: "postgres://ueyafgxlxzldeh:76ae13ca1840914ab869e18438fd0280750cd6566cc0e8f73dc588f84e016626@ec2-34-231-63-30.compute-1.amazonaws.com:5432/ddfqh165llskfc",
   entities: ["src/entities/*.{js,ts}"],
-  migrations: ["src/database/migrations/*.ts"],
+  migrations: ["src/database/migrations/*.{js,ts}"],
   logging: false,
   synchronize: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 });
